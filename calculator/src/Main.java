7eb46f560efb661c257f887127d6c23a1faf3b62
Ruts <ananimuss1551@mail.ru>
2023-10-11 16:48:45 +0300

    v1

---
+++ calculator/src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        float a = 12;
+        float b = 3;
+        
+        System.out.println(del(a,b));
+    }
+    public static float plus(float a, float b)
+    {
+        return a+b;
+    }
+    public static float minus(float a, float b)
+    {
+        return a-b;
+    }
+    public static float umnoj(float a, float b)
+    {
+        return a*b;
+    }
+    public static float del(float a, float b)
+    {
+        return a/b;
+    }
+
+}

3113f2a8dd5cb7c35eb7e3b2743700a1dbd68f80
Ruts <ananimuss1551@mail.ru>
2023-10-12 17:26:41 +0300

    v3

--- calculator/src/Main.java
+++ calculator/src/Main.java
@@ -3,43 +3,52 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Введите выражение(возможные операции: \n 1.Сложение + \n 2.Вычитане -\n 3.Умножение *\n 4. Деление /");
-        System.out.println("Пример выражения: 2 + 4");
+        System.out.println("Пример выражения: 2,3 + 4,7");
         Scanner sc = new Scanner(System.in);
-        float a = sc.nextFloat();
-        char op = sc.next().charAt(0);
-        float b = sc.nextFloat();
-        switch (op) {
-            case ('+'):
-                System.out.println(plus(a,b));
-                break;
-            case ('-'):
-                System.out.println(minus(a,b));
-                break;
-            case ('*'):
-                System.out.println(umnoj(a,b));
-                break;
-            case ('/'):
-                System.out.println(del(a,b));
-                break;
-            default:
-                System.out.println("Ошибка записи выражения");
+        float ans = sc.nextFloat();
+        while (true) {
+            char op = sc.next().charAt(0);
+            float b = sc.nextFloat();
+            switch (op) {
+                case ('+'):
+                    ans = plus(ans, b);
+                    break;
+                case ('-'):
+                    ans = minus(ans, b);
+                    break;
+                case ('*'):
+                    ans = umnoj(ans, b);
+                    break;
+                case ('/'):
+                    ans = del(ans, b);
+                    break;
+                default: {
+                    System.out.println("Ошибка записи выражения");
+                    continue;
+                }
+            }
+            if (Math.round(ans) == ans)
+                System.out.print((int) ans);
+            else
+                System.out.print(ans);
+
         }
     }
-    public static float plus(float a, float b)
-    {
-        return a+b;
+
+    public static float plus(float a, float b) {
+        return a + b;
     }
-    public static float minus(float a, float b)
-    {
-        return a-b;
+
+    public static float minus(float a, float b) {
+        return a - b;
     }
-    public static float umnoj(float a, float b)
-    {
-        return a*b;
+
+    public static float umnoj(float a, float b) {
+        return a * b;
     }
-    public static float del(float a, float b)
-    {
-        return a/b;
+
+    public static float del(float a, float b) {
+        return a / b;
     }
 
 }
